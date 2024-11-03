85e247e504535725f3cf2a965dd0f2288e074d76
SStalciuss <nojus.burdaitis@gmail.com>
2024-11-03 15:43:15 +0200

    Setup tests

--- NomNomAPI/Data/DataContext.cs
+++ NomNomAPI/Data/DataContext.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace NomNomAPI.Data
 {
-    public class DataContext : IdentityDbContext//identity padeda sukurti daug lenteliu ir reikiamos informacijos kaip roles, logins ir tt.
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-               
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -16,7 +16,7 @@ namespace NomNomAPI.Data
             optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=NomNomdb;Trusted_Connection=true;TrustServerCertificate=true");
         }
 
-        public DbSet<FoodItem> foodItems { get; set; }
+        public virtual DbSet<FoodItem> foodItems { get; set; }
         public DbSet<Store> Stores { get; set; }
     }
 }
