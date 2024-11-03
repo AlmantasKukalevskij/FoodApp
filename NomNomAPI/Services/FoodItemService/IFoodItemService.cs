e21998b22cc9de7e170c70d2c36578f79da639f2
SStalciuss <nojus.burdaitis@gmail.com>
2024-11-03 13:07:42 +0200

    remove some shit again

--- NomNomAPI/Services/FoodItemService/IFoodItemService.cs
+++ NomNomAPI/Services/FoodItemService/IFoodItemService.cs
@@ -6,10 +6,7 @@ namespace NomNomAPI.Services.FoodItemsService
     {
         Task<IEnumerable<FoodItem>> GetAllFoods();
         Task<List<FoodItem>> GetFoodsByName(string name);
-        Task<FoodItem?> GetSingleFood(int id);
         Task<List<FoodItem>> AddFood(FoodItem food);
-        Task<List<FoodItem>?> UpdateFood(int id, FoodItem request);
-        Task<List<FoodItem>?> DeleteFood(int id);
         Task<List<FoodItem>> GetAllFoodByStoreId(int storeId);
         Task<List<FoodItem>> GetFoodItemsByPriceRange(double minPrice, double maxPrice);
 
